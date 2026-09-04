df404b7cadcf5054edcd159cfa03b55fe81421e0
Rob Gage <rmg@oakleycomputing.org>
2026-09-03 23:05:13 -0400

    Create `ActorPossessable` struct

---
+++ engine_physics/src/actors/actor_possessable.rs
@@ -0,0 +1,5 @@
+// Copyright Rob Gage 2026
+
+/// Marks an actor as eligible for possession
+#[derive(bevy_ecs::component::Component)]
+pub struct ActorPossessable;
--- engine_physics/src/actors/mod.rs
+++ engine_physics/src/actors/mod.rs
@@ -2,8 +2,10 @@
 
 mod actor;
 mod actor_pawn;
+mod actor_possessable;
 mod actor_registry;
 
 pub use actor::Actor;
 pub use actor_pawn::ActorPawn;
+pub use actor_possessable::ActorPossessable;
 pub use actor_registry::ActorRegistry;
