483178d88d2bd728d86df80d19651feb57be4282
Rob Gage <rmg@oakleycomputing.org>
2026-09-03 22:42:23 -0400

    Create `ActorPawn` struct

---
+++ engine_physics/src/actors/actor_pawn.rs
@@ -0,0 +1,5 @@
+// Copyright Rob Gage 2026
+
+/// Marks an actor as a pawn
+#[derive(bevy_ecs::component::Component)]
+pub struct ActorPawn;
--- engine_physics/src/actors/mod.rs
+++ engine_physics/src/actors/mod.rs
@@ -1,7 +1,9 @@
 // Copyright Rob Gage 2026
 
 mod actor;
+mod actor_pawn;
 mod actor_registry;
 
 pub use actor::Actor;
+pub use actor_pawn::ActorPawn;
 pub use actor_registry::ActorRegistry;
