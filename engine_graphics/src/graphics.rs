fa207c598bb738517d0243ffe4d5bcb9f5ccbd3e
Rob Gage <rmg@oakleycomputing.org>
2026-09-05 01:16:03 -0400

    Create `Graphics` struct

---
+++ engine_graphics/src/graphics.rs
@@ -0,0 +1,6 @@
+// Copyright Rob Gage 2026
+
+/// Scene graphics information that is passed to the renderer
+pub struct Graphics {
+    
+}
--- engine_graphics/src/lib.rs
+++ engine_graphics/src/lib.rs
@@ -2,6 +2,8 @@
 
 mod camera;
 mod color;
+mod graphics;
 
 pub use camera::Camera;
 pub use color::Color;
+pub use graphics::Graphics;
